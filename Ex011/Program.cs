e7b10796e840b1df5ead47e29b2ae5f7b3fb825e
Hippocrates <rimo52@gmail.com>
2022-12-02 20:22:06 +0300

    Ex011: Методы заполнения и вывода массива + поиск

---
+++ Ex011/Program.cs
@@ -0,0 +1,52 @@
+﻿// Методы заполнения и вывода массива + поиск
+
+Console.Clear();
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        Console.Write(" ");
+        position++;
+    }
+}
+
+int[] array = new int[10]; //создать новый массив на 10 элементов
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // Будет возвращать позицию -1 если нет такого элемента
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
