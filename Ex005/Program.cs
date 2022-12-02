e3a47284a0d8602187c4da9de9cd5163bf084228
Hippocrates <rimo52@gmail.com>
2022-12-02 20:20:26 +0300

    Ex005: УРА, это же МАША!

---
+++ Ex005/Program.cs
@@ -0,0 +1,23 @@
+﻿// УРА, это же МАША!
+
+Console.Clear();
+
+Console.WriteLine("Введите имя пользователя: ");
+string? username = Console.ReadLine();
+
+if (String.IsNullOrEmpty(username))
+{
+    Console.WriteLine("Имя пользователя не введено");
+}
+else
+{
+    if (username.ToLower() == "маша")
+    {
+        Console.WriteLine("УРА, это же МАША!");
+    }
+    else
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+    }
+}
